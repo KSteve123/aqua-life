75543df017305f5ef4104bac7689fb6ffbd367c8
Ksteve123 <stevegame007@gmail.com>
2024-09-05 14:48:12 +0530

    remove ViewParam import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './components.js/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Check1 from './components.js/ViewParam';
 ;
 
 function App() {
